5ba25220c1041cdd3030190bd209bf3be72fb17f
amir <amyrmhmdhmzhay950@gmail.com>
2024-03-31 18:05:34 +0430

    login_user

---
+++ user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+
+    path('', views.Checklogin,name="checklogin"),
+    path('register', views.register,name="register"),
+    path('login', views.Login,name="login"),
+    path('logout', views.Logout,name="logout"),
+    path('index', views.Index,name="index"),
+    path('delete_account', views.Delete_Account,name="delete"),
+]
