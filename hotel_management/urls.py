fb7a1e320001a9cedd75d392aecd748201fe982d
barzan1380 <131408916+barzan1380@users.noreply.github.com>
2024-04-08 20:57:11 +0330

    Update urls.py

--- hotel_management/urls.py
+++ hotel_management/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import rooms_list, room_detail
 
+app_name = 'hotel'
+
 urlpatterns = [
     path('rooms/', rooms_list, name='rooms_list'),
     path('rooms/<int:room_id>/', room_detail, name='room_detail'),
-]
+    path('search/', search, name='search'),
