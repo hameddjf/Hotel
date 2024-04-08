c10a10ae7eb8c8eeba11b946b142cce42a287918
barzan1380 <131408916+barzan1380@users.noreply.github.com>
2024-04-08 20:55:56 +0330

    Update models.py
    improve models

--- hotel_management/models.py
+++ hotel_management/models.py
@@ -2,18 +2,36 @@ from django.db import models
 
 
 # Create your models here.
+
+class AvailableManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='avaliable')
+
+
 class Room(models.Model):
     STATUS_CHOICES = (
         ('avaliable', 'Available'),
         ('reserved', 'Reserved')
     )
 
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=100, verbose_name='نام')
+    description = models.TextField(blank=True, verbose_name='توضیحات')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='available')
-    area = models.CharField(max_length=30)
-    beds = models.IntegerField()
-    hotel_name = models.CharField(max_length=50)
+    area = models.CharField(max_length=30, verbose_name='منطقه')
+    beds = models.IntegerField(verbose_name='تعداد تخت')
+    hotel_name = models.CharField(max_length=50, verbose_name='هتل')
+
+    class Meta:
+        ordering = ('name',)
+        indexes = [
+            models.Index(fields=['-name'])
+        ]
 
     def __str__(self):
         return self.name
+
+
+# objects
+
+    objects = models.Manager()
+    Available = AvailableManager()
